26ae8e20043cd52e38fc5e0e210c91566d24c6f7
obaed <obaedul@redlimesolutions.com>
2024-05-08 15:34:12 +0600

    Project Basic setup done & header imntegrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
 
-function App() {
+import '@mantine/core/styles.css';
+import {  MantineProvider } from '@mantine/core';
+import { HeaderMegaMenu } from './Component/Shared/Header/HeaderMegaMenu';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <MantineProvider withGlobalClasses withCssVariables >
+          <HeaderMegaMenu></HeaderMegaMenu>
+      </MantineProvider>
     </div>
+
   );
 }
-
-export default App;
